1e2dfe6a4130d9c8c7437e855cd4a2ddbc3d215f
WantingDU <46242463+WantingDU@users.noreply.github.com>
2021-01-01 15:20:03 +0100

    Update testModel.py

--- testModel.py
+++ testModel.py
@@ -9,6 +9,8 @@ from random import random
 from operator import add
 from pyspark.sql import SparkSession
 
+
+    
 def main():
     spark = SparkSession\
         .builder\
@@ -26,15 +28,14 @@ def main():
     # print("Output details:", model.get_details()[1])
 
     # Run the model with input data
-    output=spark.read.csv(data_path).rdd.map(lambda i:model.input_data(i)).parallelize(range(1, 2), 1).map(lambda m:model.run(m)) #problem: where to place inputdata(m)
+    output=spark.read.csv(data_path).rdd.map(lambda r: r[0]).flatMap(lambda x:x.split('')).map(lambda i:model.input_data(i[0])).map(lambda m:model.run(m)) #problem: where to place inputdata(m)
     #output = model.run(input_data)
     print("*********\nOutput =\n")
     print(output)
 
-def data_from_file(data_path):
-    data = pd.read_csv(data_path)
-
-    return data
+#def data_from_file(data_path):
+    #data = pd.read_csv(data_path)
+    #return data
 
 
 if __name__ == '__main__':
