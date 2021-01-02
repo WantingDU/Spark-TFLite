a8c0b2347c246f071468ee3296464c662dfae222
WantingDU <46242463+WantingDU@users.noreply.github.com>
2021-01-02 15:12:04 +0100

    Update testModel_Spark.py

--- testModel_Spark.py
+++ testModel_Spark.py
@@ -18,37 +18,24 @@ def main():
         .appName("PredictTemperature")\
         .getOrCreate()
     data_path="/Spark-TFLite/jena_weather_dataset_roof.csv"
-    #data_raw = data_from_file(data_path)
+
     
     # Specify which model to use
     model = NeuralModel("/Spark-TFLite/model_bilstm.tflite")
 
-    # Format data
-    #input_data = model.input_data(data_raw)
-
-    # print("Output details:", model.get_details()[1])
 
     # Run the model with input data
     spark.conf.set("spark.sql.execution.arrow.enabled", "true")
-    print("000000000000000000000000")
-    #df = spark.read.csv(data_path).select('*').toPandas()
     df = pd.read_csv(data_path)
-    #csv_stream = spark.sparkContext.parallelize([csv_str.collect()[0][1]]).collect().map(lambda data:StringIO(data))
-    #df = csv_str.map(lambda data: pd.read_csv(data))
-    print("11111111111111111111111111")
     df_rdd = spark.sparkContext.parallelize([df])
-    print("yooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo")
     input_rdd = df_rdd.map(lambda i: NeuralModel("/Spark-TFLite/model_bilstm.tflite").input_data(i))
-    print("2222222222222222222222222222")
     output_rdd = input_rdd.map(lambda m: NeuralModel("/Spark-TFLite/model_bilstm.tflite").run(m))
-    print("shaaaaaaaaaaaaaaaaaaaaaaaaaabiiiiiiiiiiiiiiiiiiiiii=======================")
     output = output_rdd.collect()
     for x in output:
         print(x[0])
-    #output=spark.read.csv(data_path).rdd.map(lambda r: r[0]).flatMap(lambda x:x.split('')).map(lambda i: NeuralModel("/Spark-TFLite/model_bilstm.tflite").input_data(i[0])).map(lambda m: NeuralModel("/Spark-TFLite/model_bilstm.tflite").run(m)) #problem: where to place inputdata(m)
-    #output = model.run(input_data)
+
     print("***********************\nOutput =\n")
-    #print(output)
+
 
 
 
